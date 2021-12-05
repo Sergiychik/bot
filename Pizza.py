5aeb41886cb431109fa0ce96f15abba638d65c18
Sergiychik <91345193+Sergiychik@users.noreply.github.com>
2021-12-05 10:49:57 +0200

    Update Pizza.py

--- Pizza.py
+++ Pizza.py
@@ -1,42 +1,79 @@
-import logging
-
 from telegram.utils import helpers
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
+from telegram import (
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup, 
+    Update, 
+    ReplyKeyboardMarkup, 
+    KeyboardButton, 
+    keyboardbutton, 
+    LabeledPrice, 
+    ShippingOption, 
+    Update
+)
+from telegram.ext import (
+    Updater, 
+    CommandHandler, 
+    CallbackQueryHandler, 
+    CallbackContext, 
+    MessageHandler, 
+    Filters, 
+    PreCheckoutQueryHandler, 
+    ShippingQueryHandler, 
+    CallbackContext
+)
+from datetime import time
+import logging
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+                    )
 logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    
-    chat = update.effective_chat
+    chat_id = update.message.chat_id
+    b = time(10, 00, 00)
+    context.job_queue.run_daily(info, b, context=chat_id, name=str(chat_id))
+    update.message.reply_text('Спасибо за подписку!🥳\n Теперь я буду тебя оповещать о новых скидках и акциях😎\n\n Если не хочешь их получать, то просто отпишись😢 /stp\n\n Хочешь изменить время получения👌😁 \n /change  Вводим команду вручную\n A дальше время по гринвичу\n Например:\n/change 10 00 00 = 12:00 у нас!')
 
+    chat = update.effective_chat
     print(chat.id)
 
+def pizza(update: Update, context: CallbackContext) -> None:
     keyboard = [
-        [InlineKeyboardButton("Маргарита", callback_data='11'),InlineKeyboardButton("Четыре сыра", callback_data='22')],
-        [InlineKeyboardButton("Американо", callback_data='33'),InlineKeyboardButton("Кальцоне", callback_data='44'),InlineKeyboardButton("Падана", callback_data='55'),],
+        [InlineKeyboardButton("Маргарита", callback_data='11'),InlineKeyboardButton("Падана", callback_data='55')],
+        [InlineKeyboardButton("Американо", callback_data='33'),InlineKeyboardButton("Кальцоне", callback_data='44')],
+        [InlineKeyboardButton("Четыре сыра", callback_data='22')],
         [InlineKeyboardButton("Индивидуальный заказ", callback_data='8')],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Выберите Пиццу:', reply_markup=reply_markup)
 
+def give_data(update: Update, context: CallbackContext) -> None:
+    keyboard_r = [
+        [
+            KeyboardButton("Номер ☎️", request_contact=True),
+            KeyboardButton("Локация 🗺️", request_location=True),
+        ]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard_r)
+    update.message.reply_text("Предоставь мне свои данные для уточения заказа!🤠", reply_markup=reply_markup)
+
 def button(update: Update, context: CallbackContext) -> None:
     
     query = update.callback_query
 
-    keyboard_16p = [[InlineKeyboardButton("M\n 45см\n 60грн", callback_data='1')],
-    [InlineKeyboardButton("L\n 60см\n 80грн", callback_data='2')],
-    [InlineKeyboardButton("XL\n 90см \n 120грн", callback_data='3')],]
+    keyboard_16p = [[InlineKeyboardButton("📏Размер - 45см 💵Цена - 60грн", callback_data='111')],
+    [InlineKeyboardButton("📏Размер - 60см 💵Цена - 80грн", callback_data='222')],
+    [InlineKeyboardButton("📏Размер - 90см 💵Цена - 120грн", callback_data='333')]]
 
     reply_markup_16p = InlineKeyboardMarkup(keyboard_16p)
 
     query.answer()
     print(query.data)
+
+
 
     if query.data == '11':
         query.edit_message_text(text=f"Выбирете размер вашей пиццы", reply_markup=reply_markup_16p)
@@ -49,22 +86,25 @@ def button(update: Update, context: CallbackContext) -> None:
     elif query.data == '55':
         query.edit_message_text(text=f"Выбирете размер вашей пиццы", reply_markup=reply_markup_16p)
     elif query.data == '8':
-        query.edit_message_text(text=f"Мы перезвоним Вам в течении 5 минут")
+        query.edit_message_text(text=f"Введите свой номер☎️\n И мы перезвоним Вам в течении 5 минут\n /give_data")
     elif query.data == '1':
-        query.edit_message_text(text=f"Введите свой номер и мы Вам перезвоним!")
-    elif query.data == '2':
-        query.edit_message_text(text=f"Введите свой номер и мы Вам перезвоним!")
-    elif query.data == '3':
-        query.edit_message_text(text=f"Введите свой номер и мы Вам перезвоним!")
+        query.edit_message_text(text=f"Введите свой номер☎️ и мы Вам перезвоним! - /give_data")
+    elif query.data == '111':
+        query.edit_message_text(text=f"Отправь мне свои данные ☎️ 🗺️\n Спец команда - /give_data")
+    elif query.data == '222':
+        query.edit_message_text(text=f"Отправь мне свои данные ☎️ 🗺️\n Спец команда - /give_data")
+    elif query.data == '333':
+        query.edit_message_text(text=f"Отправь мне свои данные ☎️ 🗺️\n Спец команда - /give_data")
 
     else:
         query.edit_message_text(text=f"Ссылка на оплату")
 
 def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("/call - Для того что бы позвонить нам\n /sale - акиции\n /friend - приведи друга получи скидку 10%")
+    update.message.reply_text("/call - Для того что бы позвонить нам\n /sale - акиции\n /friend - приведи друга получи скидку 10%\n /pizza - для выбора пиццы\n /give_data - поделиться своими данными")
+
 
 def call_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("+380660137220")
+    update.message.reply_text("Вот мой номер: +380660137220\n Звони в любое рабочее время!😏\n Я всегда отечу!🤙")
 
 def sale_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("✅Покупая 5 и более штук - 1 пицца по индивидуальному заказу беспланто!\n ✅Купи сегодня пиццу с ананасом и участвуй в розиграше целого ананаса!\n ✅Завтра для всех посетителей пиццерии напитки бесплатно, при условии покупки большого стакана!")
@@ -75,25 +115,51 @@ def friend_command(update: Update, context: CallbackContext) -> None:
     text = "Вот твоя ссылка:\n\n" + url
     update.message.reply_text(text)
 
-def geophone(message) -> None:
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="номер телефона", request_contact=True)
-    button_geo = types.KeyboardButton(text="местоположение", request_location=True)
-    keyboard.add(button_phone, button_geo)
-    bot.send_message(message.chat.id, "Предоставь мне свои данные!", reply_markup=keyboard)
 
+
+def info(context: CallbackContext) -> None:
+    job = context.job
+    context.bot.send_message(job.context, text='Купи одну получи две со скидкой!')
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+def stp(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Вы успешно отписались!' if job_removed else 'Вы не подписаны на акции'
+    update.message.reply_text(text)
+
+def change(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    b = time(int(context.args[0]), int(context.args[1]), int(context.args[2]))
+    context.job_queue.run_daily(info, b, context=chat_id, name=str(chat_id))
+    text = 'Гринвич: '+ context.args[0] +' '+ context.args[1] +' '+ context.args[2]
+    if job_removed:
+        text += ' Время было успешно изменено!'
+    update.message.reply_text(text)
 
 def main() -> None:
     updater = Updater("2026797713:AAFzOVBw_3sRrpsI0QSrJx36AYdmd4oMXEA")
+    a = updater.dispatcher.add_handler
+    a(CommandHandler('start', start))
+    a(CommandHandler('pizza', pizza))
+    a(CallbackQueryHandler(button))
+    a(CommandHandler('help', help_command))
+    a(CommandHandler('call', call_command))
+    a(CommandHandler('sale', sale_command))
+    a(CommandHandler('friend', friend_command))
+    a(CommandHandler("stp", stp))
+    a(CommandHandler("change", change))
+    a(CommandHandler('give_data', give_data))
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
-    updater.dispatcher.add_handler(CommandHandler('call', call_command))
-    updater.dispatcher.add_handler(CommandHandler('sale', sale_command))
-    updater.dispatcher.add_handler(CommandHandler('friend', friend_command))
-    updater.dispatcher.add_handler(CommandHandler('geophone', geophone))
-
+    
     updater.start_polling()
     updater.idle()
 
